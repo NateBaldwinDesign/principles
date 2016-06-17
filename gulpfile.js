91632b52b3408c440d13527f3f0d7a561b47c98a
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-17 16:30:12 -0600

    updated to gulp-sass and added gulp-sourcemaps. Unable to perform sass task, still returning errors.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
 var jsonSass = require('gulp-json-sass'),
     gulp = require('gulp'),
     concat = require('gulp-concat'),
-    sass = require('gulp-ruby-sass'),
-    designProperties = require('design.json');
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    designProperties = require('./design.json');
  
 gulp.task('sass', function() {
   return gulp
-    .src( designProperties, 'example.sass')
+    .src(['design.json', 'example.scss'])
     .pipe(jsonSass({
-      sass: true
+      sass: true,
+      ignoreJsonErrors: true
     }))
     .pipe(concat('output.sass'))
-    .pipe(sass())
-    .pipe(gulp.dest('out/'));
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('example/'));
 });
