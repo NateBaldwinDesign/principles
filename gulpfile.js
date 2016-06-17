ba46234107f9c3647e1b66214fc3420ebc2e51c4
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-17 16:58:56 -0600

    linked json file to typesettings base with successful build. Broke out typesettings scss into _variables, _mixins, and _typography for easier development.

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var jsonSass = require('gulp-json-sass'),
  
 gulp.task('sass', function() {
   return gulp
-    .src(['design.json', 'example.scss'])
+    .src(['design.json', 'scss/**/*.scss'])
     .pipe(jsonSass({
       sass: true,
       ignoreJsonErrors: true
     }))
-    .pipe(concat('output.scss'))
+    .pipe(concat('style.scss'))
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('example/'));
+    .pipe(gulp.dest('dest/'));
 });
